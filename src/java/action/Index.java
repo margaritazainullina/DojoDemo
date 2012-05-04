2267682bfedaa7fbd76716473f71fe5dc158505f
Radik Zainullin <radik.zainullin@gmail.com>
2012-05-04 13:24:00 +0600

    sample001

---
+++ src/java/action/Index.java
@@ -0,0 +1,9 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Index extends ActionSupport {
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+}
